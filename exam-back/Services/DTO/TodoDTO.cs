8aa03ee74127b3fa793936c4200796a9382c7404
Mihkel Tiganik <mtiganik@gmail.com>
2024-06-02 18:34:03 +0300

    todo service

--- exam-back/Services/DTO/TodoDTO.cs
+++ exam-back/Services/DTO/TodoDTO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Identity;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Services.DTO
 {
     public class TodoDTO
     {
+        public Guid? Id { get; set; }
+        public string TaskName { get; set; } = default!;
+        public int? TaskSort { get; set; } = default!;
+        public bool IsCompleted { get; set; } = default!;
+        public Guid TodoCategoryId { get; set; } = default!;
     }
 }
--- exam-back/Services/Implementation/TodoService.cs
+++ exam-back/Services/Implementation/TodoService.cs
@@ -1,4 +1,7 @@
-﻿using Services.DTO;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using MigrationProject;
+using Services.DTO;
 using Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,29 +13,110 @@ namespace Services.Implementation
 {
     public class TodoService : ITodoService
     {
-        public Task<TodoDTO> CreateTodo(TodoDTO TodoDTO)
+        private readonly AppDbContext _context;
+        private readonly IUserGetter _userGetter;
+        public TodoService(IUserGetter userGetter, AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _userGetter = userGetter;
+
+        }
+        public async Task<TodoDTO> CreateTodo(TodoDTO TodoDTO)
+        {
+            var user = await _userGetter.GetCurrentUserAsync();
+            Guid id = (Guid)(TodoDTO.Id == null ? new Guid() : TodoDTO.Id);
+            var todo = new Todo()
+            {
+                Id = id,
+                TaskName = TodoDTO.TaskName,
+                TaskSort = TodoDTO.TaskSort,
+                AppUserId = user.Id,
+            };
+            await _context.Todos.AddAsync(todo);
+            await _context.SaveChangesAsync();
+            TodoDTO.Id = id;
+            return TodoDTO;
         }
 
-        public Task<TodoDTO> DeleteTodo(Guid Id)
+        public async Task<TodoDTO> DeleteTodo(Guid Id)
         {
-            throw new NotImplementedException();
+            var user = await _userGetter.GetCurrentUserAsync();
+            var todo = await _context.Todos.Where(u => u.Id == Id && u.AppUserId == user.Id).FirstOrDefaultAsync();
+            if (todo == null)
+            {
+                throw new Exception("Todo does not exist");
+            }
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+            return new TodoDTO()
+            {
+                TaskName = todo.TaskName,
+                TaskSort = todo.TaskSort,
+                IsCompleted = todo.IsCompleted,
+                TodoCategoryId = todo.TodoCategoryId
+            };
         }
 
-        public Task<List<TodoDTO>> GetAll()
+        public async Task<List<TodoDTO>> GetAll()
         {
-            throw new NotImplementedException();
+            var user = await _userGetter.GetCurrentUserAsync();
+            var todos = await _context.Todos.Where(u => u.AppUserId == user.Id).ToListAsync();
+            List<TodoDTO> res = new List<TodoDTO>();
+            foreach(var todo in todos)
+            {
+                var todoDTO = new TodoDTO()
+                {
+                    TaskName = todo.TaskName,
+                    TaskSort = todo.TaskSort,
+                    IsCompleted = todo.IsCompleted,
+                    TodoCategoryId = todo.TodoCategoryId,
+                    Id = todo.Id
+                };
+                res.Add(todoDTO);
+            }
+            return res;
         }
 
-        public Task<TodoDTO> GetById(Guid Id)
+        public async Task<TodoDTO> GetById(Guid Id)
         {
-            throw new NotImplementedException();
+            var user = await _userGetter.GetCurrentUserAsync();
+            var todo = await _context.Todos.Where(u => u.Id == Id && u.AppUserId == user.Id).FirstOrDefaultAsync();
+            if (todo == null)
+            {
+                throw new Exception($"No todo found with id {Id}");
+            }
+            return new TodoDTO()
+            {
+                TaskName = todo.TaskName,
+                TaskSort = todo.TaskSort,
+                IsCompleted = todo.IsCompleted,
+                TodoCategoryId = todo.TodoCategoryId,
+                Id = todo.Id
+            };
         }
 
-        public Task<TodoDTO> Update(TodoDTO TodoDTO)
+        public async Task<TodoDTO> Update(TodoDTO TodoDTO)
         {
-            throw new NotImplementedException();
+            var user = await _userGetter.GetCurrentUserAsync();
+            var todo = await _context.Todos.Where(u => u.Id == TodoDTO.Id && u.AppUserId == user.Id).FirstOrDefaultAsync();
+            if (todo == null)
+            {
+                throw new Exception($"No todo found with id {TodoDTO.Id}");
+            }
+            todo.TaskName = TodoDTO.TaskName;
+            todo.TodoCategoryId = TodoDTO.TodoCategoryId;
+            todo.IsCompleted = TodoDTO.IsCompleted;
+            todo.TaskSort = TodoDTO.TaskSort;
+            await _context.SaveChangesAsync();
+            return new TodoDTO()
+            {
+                TaskName = todo.TaskName,
+                TaskSort = todo.TaskSort,
+                IsCompleted = todo.IsCompleted,
+                TodoCategoryId = todo.TodoCategoryId,
+                Id = todo.Id
+            };
+
         }
     }
 }
